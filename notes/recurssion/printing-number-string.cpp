4869a5c973bebcfe0f4215a50505ee090d29e964
DITNO <92884446+DITNO@users.noreply.github.com>
2023-06-25 16:03:43 +0530

    Create printing-number-string.cpp
    2345 - two three four five

---
+++ notes/recurssion/printing-number-string.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+    string arr[] = {
+        "zero","one","two","three","four","five","six","seven","eight","nine"
+    };
+
+    
+void print(int n){
+    //base case
+    if(n == 0){
+        return;
+    }
+    
+    //rec case
+    print(n/10);
+    cout<<arr[n%10]<<" ";
+}
+
+int main() {
+    
+    int n;
+    cin>>n;
+    if(n == 0){
+        cout<<"zero";
+    }
+    print(n);
+
+    return 0;
+}
